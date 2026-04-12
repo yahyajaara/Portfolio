0ba945195a4dd14531350ba1b2e252b9304b2bb7
yahyajaara <yahyajaara1411@gmail.com>
2026-04-13 01:49:30 +0300

    Create a PWA System

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "portfolio-cache-v1";
+
+const urlsToCache = [
+  "/Portfolio/",
+  "/Portfolio/index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
